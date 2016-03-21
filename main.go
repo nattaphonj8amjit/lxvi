8b361e64555bf633da00c01bc08dcff030dceebd
nattaphon <kmutnb.smcs.ball@gmail.com>
2016-03-22 00:03:09 +0700

    MAC

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 	"net/http"
 )
+
+type Person struct {
+	Name  string
+	Phone string
+}
 
 func main() {
 
 	// init
-	//gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
 	router.Static("/js", "./js")
@@ -21,6 +30,36 @@ func main() {
 		})
 	})
 
-	router.Run(":80")
+	router.GET("/mongo", func(c *gin.Context) {
+		mongo()
+	})
 
+	router.Run(":8081")
+
+}
+
+func mongo() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
 }
