8e67092180fdfe7b8b2e88d0ffa64de640cbb8c1
nattaphon <kmutnb.smcs.ball@gmail.com>
2016-05-07 17:16:10 +0700

    sat may 7 17.16

--- main.go
+++ main.go
@@ -9,15 +9,60 @@ import (
 	"net/http"
 )
 
-type Person struct {
-	Name  string
-	Phone string
+type MyCollection struct {
+	*mgo.Collection
+}
+
+func (mc *MyCollection) findAllGoods() (response []goods) {
+	err := mc.Find(bson.M{}).Sort("price").All(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("response", len(response), " doc")
+	return response
+}
+
+func (mc *MyCollection) findAllGoodsByCollection(goodsCollection string) (response []goods) {
+	mc.EnsureIndexKey("collection")
+	err := mc.Find(bson.M{"collection": goodsCollection}).Sort("name").All(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("response", len(response), " doc")
+	return response
+}
+
+func (mc *MyCollection) findOneGoodsById(goodsId string) (response goods) {
+	mc.EnsureIndexKey("goodsId")
+	err := mc.FindId(bson.ObjectIdHex(goodsId)).One(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return response
+}
+
+type goods struct {
+	Id         bson.ObjectId "_id"
+	Collection string
+	Name       string
+	Code       string
+	Price      float32
+	Desc       string
+	Detail     string
+	ShipDetail string
+	Option     []option
+}
+
+type option struct {
+	Name   string
+	Price  float32
+	Choice []int
 }
 
 func main() {
 
 	// init
-	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
 	router.Static("/js", "./js")
@@ -30,36 +75,56 @@ func main() {
 		})
 	})
 
-	router.GET("/mongo", func(c *gin.Context) {
-		mongo()
+	router.GET("/findAll", func(c *gin.Context) {
+		//size := c.DefaultQuery("size", ".")
+
+		session, err := mgo.Dial("128.199.245.83")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		prod := session.DB("prod")
+		prod.Login("admin", "Ball0931372529")
+		mc := &MyCollection{prod.C("goods")}
+		c.JSON(http.StatusOK, mc.findAllGoods())
+
 	})
 
+	router.GET("/findAllGoodsByCollection/:goodsCollection", func(c *gin.Context) {
+		goodsCollection := c.Param("goodsCollection")
+
+		session, err := mgo.Dial("128.199.245.83")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		prod := session.DB("prod")
+		prod.Login("admin", "Ball0931372529")
+		mc := &MyCollection{prod.C("goods")}
+		c.JSON(http.StatusOK, mc.findAllGoodsByCollection(goodsCollection))
+
+	})
+
+	router.GET("/findOneGoodsById/:goodsId", func(c *gin.Context) {
+		goodsId := c.Param("goodsId")
+		session, err := mgo.Dial("128.199.245.83")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		prod := session.DB("prod")
+		prod.Login("admin", "Ball0931372529")
+		mc := &MyCollection{prod.C("goods")}
+		c.JSON(http.StatusOK, mc.findOneGoodsById(goodsId))
+
+	})
 	router.Run(":8081")
 
 }
 
-func mongo() {
-	session, err := mgo.Dial("localhost")
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
 
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
 
-	c := session.DB("test").C("people")
-	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-		&Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result := Person{}
-	err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Phone:", result.Phone)
-}
+//db.C(Collection).Find(Query).Limit(limit).Sort(sortBy...)
